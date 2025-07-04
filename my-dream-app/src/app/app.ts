904bf5d478ff5d0c515f13e0d0eb3d7f5b89e679
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-07-03 20:13:04 -0500

    Implementar enlace bidireccional de datos en un componente standalone

--- my-dream-app/src/app/app.ts
+++ my-dream-app/src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected title = 'my-dream-app';
+export class AppComponent {
+  title = 'my-dream-app';
+  name: string = "Santiago Enrique";
+  age: number = 40;                 
 }
