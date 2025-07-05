717f395441feff309b256dba2fa1aa94fa81f0a8
santiagopalma12 <spalmaa@unsa.edu.pe>
2025-07-04 21:04:19 -0500

    Integrar Bootstrap e implementar barra de navegación responsiva

--- my-dream-app/src/app/app.ts
+++ my-dream-app/src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router'; 
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 @Component({
-      selector: 'app-root',
-      standalone: true,
-      imports: [CommonModule, RouterOutlet, RouterLink], 
-      templateUrl: './app.html',
-      styleUrls: ['./app.css']
-    })
-    export class AppComponent {
-      title = 'my-dream-app';
-    }
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class AppComponent {
+  title = 'my-dream-app';
+}
